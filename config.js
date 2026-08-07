b63a3c6cd850600d72458fbe1daeae067ba8b73a
ConvitePremium <essenciamusicalofc@gmail.com>
2026-08-07 01:48:25 -0300

    Update config.js

--- config.js
+++ config.js
@@ -37,7 +37,7 @@ window.CONFIG = {
   // ----- LOCALIZAÇÃO (BOTÃO MAPA) ----------------------------------------
   // Link completo do Google Maps. Abra o local no Maps, clique em "Compartilar"
   // -> "Copiar link" e cole aqui (mantenha as aspas).
-  mapa: "google.com/maps/place/Di+Gregório+Buffet/data=!4m2!3m1!1s0x7c74963cf20627d:0x93d404eea6894525?sa=X&ved=1t:242&ictx=111",
+  mapa: "https://maps.app.goo.gl/nmDYDpML4RMhSazY8",
 
   // ----- QUAIS TELAS APARECEM --------------------------------------------
   // video:     true  -> toca o vídeo (assets/video.mp4) depois da abertura.
